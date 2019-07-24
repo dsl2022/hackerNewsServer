2f3654081eb113539dc0944cdf41fb1080c194b7
JizongL <2015rpro@gmail.com>
2019-07-24 10:18:22 -0400

    installed the prisma client

--- src/index.js
+++ src/index.js
@@ -1,42 +1,57 @@
 const {GraphQLServer} = require('graphql-yoga')
 
-const typeDefs = `
-    type Query{
-        info: String!
-        feed: [Link!]!
-    }
 
-    type Mutation {
-        post(url: String!, description:String!):Link!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url:String!
-    }
-`
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
   }]
 
+let idCount = links.length
 const resolvers = {
     Query:{
         info:()=> `This is the API of a Hackernews Clone`,
+        link:(id)=>links[id],
         feed: ()=>links,
     },
 
-    Link:{
-        id:(parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
+    Mutation:{
+        post:(parent,args)=>{
+            const link = {
+                id:`link-${idCount++}`,
+                description:args.description,
+                url:args.url,
+            }
+            links.push(link)            
+            return link
+        },
+        updateLink:(parent,args)=>{
+            const link = {  
+                id:`link-${args.id}`,              
+                description:args.description,
+                url:args.url,
+            }
+            links[Number(args.id)]=link
+            console.log(links,'test link')
+            return link
+            
+        },
+        deleteLink:(parent,args)=>{
+            console.log(args)
+            console.log(links,'before')
+            if(links.length===1){
+                links = []
+            }
+            links = links.slice(0,args.id-1).concat(links.slice(args.id,links.length))
+            console.log(links,'after')
+            // return `link ${args.id} is deleted`
+        }
+    },
+        
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 
---
+++ src/note.md
@@ -0,0 +1,28 @@
+### Temp note for syntax for playground 
+
+for updateLink 
+
+```
+mutation{
+ updateLink(
+  id:3
+  url:"test"
+  description:"test"
+  
+){
+   id
+  }
+}
+```
+
+
+```
+mutation{
+ deleteLink(  
+  id:9  
+){
+   id
+  }
+}
+
+```
